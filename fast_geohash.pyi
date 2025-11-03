d2c80d0680a60c90defc7252c498030cb82668ab
Eashwar Ranganathan <eashwar@eashwar.com>
2025-11-02 16:22:57 -0800

    Switch coordinates from (x, y) to (lat, lng) ordering

--- fast_geohash.pyi
+++ fast_geohash.pyi
@@ -1,7 +1,7 @@
 def encode(coords: tuple[float, float], /, precision: int = 12) -> str:
 	"""Encodes a coordinate point into a geohash
 
-	:param coords: A coordinate tuple of (longitude, latitude)
+	:param coords: A coordinate tuple of (latitude, longitude)
 	:type coords: tuple[float, float]
 	:param precision: The precisions/length of the output geohash from
 		1 to 12 inclusive (defaults to 12)
@@ -17,7 +17,7 @@ def decode_bbox(hash_str: str) -> tuple[tuple[float, float], tuple[float, float]
 	:param hash_str: The geohash string
 	:type hash_str: str
 	:returns: The bounding box as a (min_coord, max_coord) tuple where
-		each coord is a tuple of (longitude, latitude)
+		each coord is a tuple of (latitude, longitude)
 	:rtype: tuple[tuple[float, float], tuple[float, float]]
 	"""
 	...
@@ -27,8 +27,8 @@ def decode_exact(hash_str: str) -> tuple[tuple[float, float], float, float]:
 
 	:param hash_str: The geohash string
 	:type hash_str: str
-	:returns: A tuple of (coordinate, longitude_error, latitude_error) where
-		the coordinate is a tuple of (longitude, latitued) and each error value
+	:returns: A tuple of (coordinate, latitude_error, longitude_error) where
+		the coordinate is a tuple of (latitude, longitude) and each error value
 		is a float
 	:rtype: tuple[tuple[float, float], float, float]
 	"""
@@ -39,9 +39,9 @@ def decode(hash_str: str) -> tuple[str, str]:
 
 	:param hash_str: The geohash string
 	:type hash_str: str
-	:returns: The coordinate as a tuple of strings (longitude, latitude)
+	:returns: The coordinate as a tuple of strings (latitude, longitude)
 		with each string represnentation truncated to a precision matching
-		the error margins
+		the error margins and trailing zeroes removed
 	:rtype: tuple[str, str]
 	"""
 	...
--- tests/test_fast_geohash.py
+++ tests/test_fast_geohash.py
@@ -4,52 +4,52 @@ import fast_geohash
 
 
 def test_encode_explicit_len() -> None:
-	result = fast_geohash.encode((112.5584, 37.8324), precision=9)
+	result = fast_geohash.encode((37.8324, 112.5584), precision=9)
 
 	assert result == 'ww8p1r4t8'
 
 
 def test_encode_default_len() -> None:
-	result = fast_geohash.encode((112.5584, 37.8324))
+	result = fast_geohash.encode((37.8324, 112.5584))
 
 	assert result == 'ww8p1r4t8yd0'
 
 
 def test_encode__invalid_coordinates() -> None:
 	with pytest.raises(ValueError) as e:
-		fast_geohash.encode((float('nan'), 37.8324), 9)
+		fast_geohash.encode((float('nan'), 112.5584), 9)
 
-	assert str(e.value) == 'invalid coordinate range: COORD(NaN 37.8324)'
+	assert str(e.value) == 'invalid coordinate range: COORD(lat=NaN, lng=112.5584)'
 
 
 def test_encode__invalid_length() -> None:
 	with pytest.raises(ValueError) as e:
-		fast_geohash.encode((112.5584, 37.8324), 14)
+		fast_geohash.encode((37.8324, 112.5584), 14)
 
 	assert str(e.value) == 'Invalid length specified: 14. Accepted values are between 1 and 12, inclusive'
 
 
 def test_decode_bbox() -> None:
 	assert fast_geohash.decode_bbox('ww8p1r4t8') == (
-		(112.55836486816406, 37.83236503601074),
-		(112.5584077835083, 37.83240795135498),
+		(37.83236503601074, 112.55836486816406),
+		(37.83240795135498, 112.5584077835083),
 	)
 
 
 def test_decode_exact() -> None:
 	assert fast_geohash.decode_exact('ww8p1r4t8') == (
-		(112.55838632583618, 37.83238649368286),
+		(37.83238649368286, 112.55838632583618),
 		2.1457672119140625e-05,
 		2.1457672119140625e-05,
 	)
 
 
 def test_decode() -> None:
-	assert fast_geohash.decode('ww8p1r4t8') == ('112.5584', '37.8324')
+	assert fast_geohash.decode('ww8p1r4t8') == ('37.8324', '112.5584')
 
 
 def test_round_trip() -> None:
-	coord = (112, 37)
+	coord = (37, 112)
 	geohash = fast_geohash.encode(coord, precision=12)
 	decoded = fast_geohash.decode(geohash)
 
